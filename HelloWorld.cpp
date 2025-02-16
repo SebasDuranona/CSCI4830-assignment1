cf744f08ea967d6fd9eedc77092a9fa37c28db73
Sebas Duranona <sduranona97@gmail.com>
2025-02-15 21:12:32 -0600

    Adding author name

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World" 
-//Author: Jaydeb Sarker <jsarker@unomaha.edu>
+//Author: Sebastian Gomez-Duranona <sgomezduranonapoun@unomaha.edu>
 // Header file for input output functions 
 #include<iostream>  
   
